2ffd1049bb3ff8187de9025afd4d586ef9ce041f
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2022-06-13 12:24:40 +0200

    Support sending a content length.
    We've switched to ZipStream which does not support async io but which is
    able to calculate the length (as long as the zip file is not compressed,
    which it isn't since it contains only JPEGs).

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import gzip
 import json
+from datetime import tzinfo, timezone
+from email.utils import format_datetime
 
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 from urllib.parse import urlparse, unquote
 from pathlib import Path
-from zipstream import AioZipStream
+from zipstream.ng import ZipStream
 
 app = FastAPI()
 
@@ -18,22 +20,20 @@ with gzip.open("downloads.json.gz", "rt") as f:
 @app.get("/downloads/facsimiles/{sigil}.zip",
          responses={200: { 'content': {'application/zip': {}}}},
          description="Stream a ZIP file with all facsimiles of that sigil that are available for download.")
-async def get_sigil(sigil: str) -> StreamingResponse:
+def get_sigil(sigil: str) -> StreamingResponse:
     try:
         pages = downloads[sigil]
-        spec = []
+        zs = ZipStream(sized=True)
         for page, images in pages.items():
             for variant, url in enumerate(images):
                 suffix = '' if variant == 0 else chr(ord('a') + variant - 1)  # some pages have more than one image
-                spec.append({
-                    'file': IMAGE_ROOT + unquote(urlparse(url).path), # JSON contains an URL, we need a path
-                    'name': f'{sigil}/{sigil}-{int(page):03}{suffix}.jpg',         # new file name
-                    'compression': None
-                })
-        zipstream = AioZipStream(spec)
-        response = StreamingResponse(zipstream.stream(),
+                zs.add_path(IMAGE_ROOT + unquote(urlparse(url).path), f'{sigil}/{sigil}-{int(page):03}{suffix}.jpg')
+
+        response = StreamingResponse(iter(zs),
                                      media_type='application/zip',
-                                     headers={'Content-Disposition': f'attachment; filename="{sigil}.zip"'})
+                                     headers={'Content-Disposition': f'attachment; filename="{sigil}.zip"',
+                                              'Content-Length': str(len(zs)),
+                                              'Last-Modified': format_datetime(zs.last_modified.astimezone(timezone.utc), usegmt=True)})
         return response
     except KeyError:
         raise HTTPException(status_code=404, detail=f"{sigil} is not a valid sigil.")
