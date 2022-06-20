acaf7f3777ac9a76f37216dd39d25f3887f79278
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2022-06-20 12:42:55 +0200

    configurability via .env file

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import gzip
 import json
-from datetime import tzinfo, timezone
+from datetime import timezone
 from email.utils import format_datetime
 
+from pydantic import BaseSettings
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 from urllib.parse import urlparse, unquote
-from pathlib import Path
 from zipstream.ng import ZipStream
 
+
+class Settings(BaseSettings):
+    image_root: str = '/faust'
+    downloads_file: str = "downloads.json.gz"
+    app_root: str = "/downloads/facsimiles"
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
 app = FastAPI()
 
-IMAGE_ROOT = '/faust'
-
-with gzip.open("downloads.json.gz", "rt") as f:
+with gzip.open(settings.downloads_file, "rt") as f:
     downloads = json.load(f)
 
 
-@app.get("/downloads/facsimiles/{sigil}.zip",
-         responses={200: { 'content': {'application/zip': {}}}},
+@app.get(settings.app_root + "/{sigil}.zip",
+         responses={200: {'content': {'application/zip': {}}}},
          description="Stream a ZIP file with all facsimiles of that sigil that are available for download.")
 def get_sigil(sigil: str) -> StreamingResponse:
     try:
@@ -27,13 +36,15 @@ def get_sigil(sigil: str) -> StreamingResponse:
         for page, images in pages.items():
             for variant, url in enumerate(images):
                 suffix = '' if variant == 0 else chr(ord('a') + variant - 1)  # some pages have more than one image
-                zs.add_path(IMAGE_ROOT + unquote(urlparse(url).path), f'{sigil}/{sigil}-{int(page):03}{suffix}.jpg')
+                zs.add_path(settings.image_root + unquote(urlparse(url).path),
+                            f'{sigil}/{sigil}-{int(page):03}{suffix}.jpg')
 
         response = StreamingResponse(iter(zs),
                                      media_type='application/zip',
                                      headers={'Content-Disposition': f'attachment; filename="{sigil}.zip"',
                                               'Content-Length': str(len(zs)),
-                                              'Last-Modified': format_datetime(zs.last_modified.astimezone(timezone.utc), usegmt=True)})
+                                              'Last-Modified': format_datetime(
+                                                  zs.last_modified.astimezone(timezone.utc), usegmt=True)})
         return response
     except KeyError:
         raise HTTPException(status_code=404, detail=f"{sigil} is not a valid sigil.")
